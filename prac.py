5e313a1bbe8ead1cc913b4e0e6cf8462820998bc
Dean Chap <deanchaps@gmail.com>
2023-03-13 18:07:05 -0230

    update

--- prac.py
+++ prac.py
@@ -22,3 +22,17 @@ figure = px.line(data, x=matches, y="Runs",
                  title='Runs Scored by Virat Kohli Between 18-Aug-08 - 22-Jan-17')
 #figure.show()
 
+data["Pos"] = data["Pos"].map({3.0: "Batting At 3", 4.0: "Batting At 4", 2.0: "Batting At 2", 
+                               1.0: "Batting At 1", 7.0:"Batting At 7", 5.0:"Batting At 5", 
+                               6.0: "batting At 6"})
+
+Pos = data["Pos"].value_counts()
+label = Pos.index
+counts = Pos.values
+colors = ['gold','lightgreen', "pink", "blue", "skyblue", "cyan", "orange"]
+
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Number of Matches At Different Batting Positions')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30,
+                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.show()
